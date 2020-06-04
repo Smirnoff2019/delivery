c90306ef7dfc0747abbff93b7e2d1d9acc7403e6
smirnoff <smirnoff.mu@gmail.com>
2020-06-04 18:10:12 +0300

    REST API

--- backend/django_delivery/django_delivery/urls.py
+++ backend/django_delivery/django_delivery/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Delivery API",
       default_version='v1',
-      description=''' 
+      description='''
           Documentation `ReDoc` view can be found [here](/doc).
       ''',
       contact=openapi.Contact(email="smirnoff.mu@gmail.com"),
