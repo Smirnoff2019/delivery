2a046c65a72786a1f4d09daf69b4d6d189ee669c
smirnoff <smirnoff.mu@gmail.com>
2020-06-08 12:19:22 +0300

    about_us and contact

--- backend/django_delivery/market/urls.py
+++ backend/django_delivery/market/urls.py
@@ -4,10 +4,12 @@ from market.views.product import ProductListView
 from market.views.consumer import ConsumerListView
 from market.views.provider import ProviderListView
 
+from market.views.auth import about_us, contacts
 
 urlpatterns = [
     path('userlogin/', AuthView.as_view()),
-    # path('hello/', hello),
+    path('about_us/', about_us),
+    path('contacts/', contacts),
     path('product_list/', ProductListView.as_view()),
     path('consumer_list/', ConsumerListView.as_view()),
     path('provider_list/', ProviderListView.as_view())
--- backend/django_delivery/market/views/auth.py
+++ backend/django_delivery/market/views/auth.py
@@ -1,5 +1,6 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -30,6 +31,19 @@ class AuthView(APIView):
         }).data)
 
 
-# @api_view()
-# def hello(request):
-#     return Response({"message": "hello"})
+@api_view()
+def about_us(request):
+    return Response({"title": "Будинок іграшок",
+                     "description": "«Будинок іграшок» – крупнейшая украинская сеть магазинов игрушек. "
+                                    "Наша цель - дарить, удивлять и немножечко дуреть, наполняя жизнь позитивными эмоциями. "
+                                    "Мы верим, что взрослых не существует, поэтому ассортимент магазина «Будинок іграшок» настолько широк, что найти подходящую игрушку можно как для новорожденного ребенка, так и для взрослого."})
+
+
+@api_view()
+def contacts(request):
+    return Response({
+        "title": "Контактна інформація Radio ROKS",
+        "description": "Центральний офіс "
+                       "04080, Київ, вул. Вікентія Хвойки, 15/15 "
+                       "Телефон / факс: +380 (44) 207-39-90 "
+                       "Ел. пошта: info@radioroks.ua"})
